7d2a6e5a35de15daf1c4248fbf155dcd15040a42
Hem4114 <152297052+Hem4114@users.noreply.github.com>
2024-10-10 19:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 let turn='O';
-let i=0;
-function fun(id)
+let t=0;
+let a=[['a','b','c'],['d','e','f'],['g','h','i']];
+turn='O';
+function fun(i,j,id)
 {
+    a[i][j]=turn;
     let c=document.getElementById(id).textContent;
-    if(c =='' && i<9)
-        {
+    if(c =='' && t<9)
+    {   
         if(turn=='O')
         {
             document.getElementById(id).innerHTML=turn;
             turn='X';
         }
-        else
+        else if(turn=='X')
         {
             document.getElementById(id).innerHTML=turn;
             turn='O';
         }
+    
+        if((a[0][0]==a[0][1] && a[0][1]==a[0][2]) || (a[1][0]==a[1][1] && a[1][1]==a[1][2]) || (a[2][0]==a[2][1] && a[2][1]==a[2][2]) || (a[0][0]==a[1][1] && a[1][1]==a[2][2]) || (a[0][2]==a[1][1] &&a[1][1]==a[2][0]) || (a[0][0]==a[1][0] && a[1][0]==a[2][0]) || (a[0][1]==a[1][1] && a[1][1]==a[2][1]) || (a[0][2]==a[1][2] && a[1][2]==a[2][2]))
+        {
+            if(turn=='X')
+            {
+                window.alert("O wins");
+                location.reload();
+            }
+            else{
+                window.alert("X wins");
+                location.reload();
+            }
+        }
+        t++;
+        if(t==9)
+        {
+            location.reload();
+        }
         
-        i++;
     }
-    if(i==9)
-    {
-        window.alert("Game is tie");
-    }
+    
 }
